8702deb8462df5f44114832434237b818d19a49d
Nick Gaswirth <nick@roundhouse-designs.com>
2023-02-10 09:51:31 -0500

    Manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { dependencies } from './package.json';
 
 // import * as path from 'path';
 
@@ -8,6 +9,17 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ['react', 'react-router-dom', 'react-dom'],
+					...renderChunks(dependencies),
+				},
+			},
+		},
+	},
 	// FIXME VSCode can't follow the alias here for autocomplete, definitions, etc
 	// resolve: {
 	// 	alias: {
@@ -15,3 +27,12 @@ export default defineConfig({
 	// 	},
 	// },
 });
+
+function renderChunks(deps: Record<string, string>) {
+	let chunks = {};
+	Object.keys(deps).forEach((key) => {
+		if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+		chunks[key] = [key];
+	});
+	return chunks;
+}
