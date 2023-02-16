8e61d75784c4007168207f5e2579d510b3c509c4
Nick Gaswirth <nick@roundhouse-designs.com>
2023-02-16 10:12:53 -0500

    Unused imports

--- src/hooks/queries/useUserProfile.tsx
+++ src/hooks/queries/useUserProfile.tsx
@@ -3,8 +3,6 @@
  */
 
 import { gql, useQuery } from '@apollo/client';
-import { Credit, UserProfile } from '../../lib/classes';
-import { CreditParams } from '../../lib/types';
 
 const QUERY_USER = gql`
 	query UserQuery($last: Int = 5, $id: ID!, $author: Int!) {
