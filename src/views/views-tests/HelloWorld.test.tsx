4336a92d6d18e8e57578fc4d9e3736ea5cba6ed9
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-03-20 11:04:08 -0700

    tests running - must wrap all testing react component with MockedProvider>

--- src/views/views-tests/HelloWorld.test.tsx
+++ src/views/views-tests/HelloWorld.test.tsx
@@ -1,10 +1,15 @@
-import { describe, test, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { HelloWorld } from './helloWorld';
+import { MockedProvider } from '@apollo/client/testing';
+import { HelloWorld } from '../helloWorld';
 
 describe('Hello World', () => {
 	it('displays "Hello World!"', () => {
-		render(<HelloWorld />);
+		render(
+			<MockedProvider>
+				<HelloWorld />
+			</MockedProvider>
+		);
 		const linkElement = screen.getByText(/Hello World!/i);
 		expect(linkElement).toBeInTheDocument();
 	});
