3318cd09c3455e174a6c6efab34215853a10dbca
Nick Gaswirth <665784+gaswirth@users.noreply.github.com>
2023-03-21 11:30:05 -0400

    Update SearchWizardView.tsx
    Search support message

--- src/views/SearchWizardView.tsx
+++ src/views/SearchWizardView.tsx
@@ -34,6 +34,10 @@ export default function SearchWizardView({ showButtons, onSubmit }: Props) {
 
 	return (
 		<form id='search-candidates' onSubmit={onSubmit}>
+			<Text variant='devAlert'>
+				Search is under development and the interface is for testing purposes only. Results are not
+				guaranteed to be accurate.
+			</Text>
 			<Stack direction='column' gap={8}>
 				{/* TODO convert Department, Jobs, and Skills to unified <CheckboxButtonFilterGroup> interface */}
 				{/* Step 1 */}
