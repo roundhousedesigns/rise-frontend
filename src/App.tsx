9e0dfbf5094b56ed236762e52e2818038db62e6c
Nick Gaswirth <nick@roundhouse-designs.com>
2023-05-07 02:52:26 -0400

    Clearus cruftus

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,11 @@ import { FiMoon, FiSun } from 'react-icons/fi';
 import { SearchContextProvider } from './context/SearchContext';
 
 import { useViewer } from './hooks/queries/useViewer';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 import LoginView from './views/LoginView';
 
 export default function App() {
 	const { loggedInId } = useViewer();
 	const { colorMode, toggleColorMode } = useColorMode();
-
-	const navigate = useNavigate();
-
-	// get the current route
-	// const { pathname } = useLocation();
-
-	// const publicEndpoints = ['/register', '/login', '/lost-password', '/reset-password'];
-
-	// if the user is not logged in, redirect to the login page
-	// useEffect(() => {
-	// 	if (!loggedInId && !publicEndpoints.includes(pathname)) {
-	// 		navigate('/login');
-	// 	}
-	// }, [loggedInId, pathname]);
 
 	return (
 		<SearchContextProvider>
