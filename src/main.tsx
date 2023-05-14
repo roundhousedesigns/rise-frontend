48967ab186e0b756089c707bc4f80815ec9cc64d
Nick Gaswirth <nick@roundhouse-designs.com>
2023-05-14 10:49:56 -0400

    Dashboard placeholder; GA4

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
 
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-
 
 import theme from './theme/index';
 import Fonts from './theme/Fonts';
@@ -14,8 +14,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
 const backend = import.meta.env.VITE_BACKEND_URL ? import.meta.env.VITE_BACKEND_URL : '';
+
+// Initialize Google Analytics
+ReactGA.initialize('G-RBR5GCNF2R');
 
 /**
  * Apollo client.
@@ -44,7 +46,11 @@ root.render(
 	</StrictMode>
 );
 
+const sendAnalytics = () => {
+	ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendAnalytics);
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,68 +1,10 @@
-import { Heading, Text, Card, Wrap, UnorderedList, ListItem, Link, Box } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 import Page from '../components/Page';
 
 export default function Dashboard() {
 	return (
 		<Page title='Hello!'>
-			<Card>
-				<Wrap>
-					<Text pb={6}>
-						Thank you for being a test user for the Alpha version of the directory! Some things to
-						keep in mind:
-					</Text>
-					<Text pb={4}>
-						This website and directory is very much still a work in progress, so please be aware of
-						that as you are using it and creating a profile. If any bugs or issues arise while
-						completing the profile please please email them to us at{' '}
-						<Link color='brand.orange' href='mailto:info@risetheatre.org'>info@risetheatre.org</Link>.
-					</Text>
-					<Text>
-						After completion of your user profile please complete{' '}
-						<Link
-							href='https://docs.google.com/forms/d/e/1FAIpQLScIdkuuoGHWuULA6bI1l5zvaLIaT6KQaK6SznmLMGGQvLzrUQ/viewform?usp=sf_link'
-							color='brand.orange'
-						>
-							this google form
-						</Link>{' '}
-						to provide feedback on your experience.
-					</Text>
-					<Box pt={8}>
-						<Heading size='lg' my={4}>
-							Some other things to keep in mind...
-						</Heading>
-						<UnorderedList listStylePos='inside' spacing={2}>
-							<ListItem fontWeight='bold'>This directory is under ACTIVE DEVELOPMENT.</ListItem>
-							<ListItem>
-								The site and interface may break layout and/or alignment, especially on smaller
-								devices.
-							</ListItem>
-							<ListItem>
-								You will likely encounter bugs, and we'd like you to let us know when you find one!
-							</ListItem>
-							<ListItem>
-								The Search funtionality is limited, as there are currently only a handful of users.
-								We'd like you to focus on the profile screens, though you are welcome to click
-								anything and everything you see.
-							</ListItem>
-						</UnorderedList>
-					</Box>
-				</Wrap>
-
-				<Wrap mt={8}>
-					<Heading as='h4' size='lg'>
-						Instructions:
-					</Heading>
-					<Box>
-						<Text>
-							You can visit your profile by clicking the gray User icon on the right side of the
-							site header, and you can start editing your profile by clicking the "Edit" button at
-							the top of the page. You can search for other members by using the Search icon in the
-							site header, and by clicking through the search prompts, you'll be able to granularly
-							select candidates with those attributes and experience points you choose.
-						</Text>
-					</Box>
-				</Wrap>
-			</Card>
+			<Heading>Welcome to RISE!</Heading>
 		</Page>
 	);
 }
