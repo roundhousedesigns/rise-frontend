f27cd0e1ab0adde133c3ebc3e2ef523742b7d9ff
Nick Gaswirth <nick@roundhouse-designs.com>
2023-05-18 01:53:00 -0400

    Help page

--- src/routes/Help.tsx
+++ src/routes/Help.tsx
@@ -1,5 +1,13 @@
 import Page from '../components/Page';
+import { usePostContent } from '../hooks/queries/usePostContent';
+import { Container, Spinner } from '@chakra-ui/react';
 
 export default function Help() {
-	return <Page title="Help">Documentation coming soon!</Page>;
+	const [content, { contentLoading, contentError }] = usePostContent('959');
+
+	return (
+		<Page title='Help'>
+			<Container maxW='4xl'>{contentLoading && !contentError ? <Spinner /> : content}</Container>
+		</Page>
+	);
 }
