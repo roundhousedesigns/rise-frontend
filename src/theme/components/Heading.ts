15ad8052f1c773d87955e1417a8499bf1b2d814d
Nick Gaswirth <nick@roundhouse-designs.com>
2023-06-08 17:18:00 -0400

    defineStyle

--- src/theme/components/Heading.ts
+++ src/theme/components/Heading.ts
@@ -1,49 +1,51 @@
-import { defineStyleConfig } from '@chakra-ui/react';
+import { defineStyle, defineStyleConfig } from '@chakra-ui/react';
 
-// TODO use defineStyle
+const baseStyle = defineStyle({
+	mb: 2,
+	fontWeight: 'normal',
+});
+
+const variants = {
+	pageTitle: defineStyle({
+		fontSize: '4xl',
+	}),
+	pageSubtitle: defineStyle({
+		fontSize: '3xl',
+	}),
+	contentTitle: defineStyle({
+		fontSize: '2xl',
+		pb: 0,
+	}),
+	contentSubtitle: defineStyle({
+		fontSize: 'md',
+		fontWeight: 'medium',
+		mt: 0,
+		pb: 0,
+	}),
+	centerline: defineStyle({
+		size: 'lg',
+		_light: {
+			bg: 'gray.50',
+		},
+		_dark: {
+			bg: 'gray.900',
+			color: 'text.light',
+		},
+		display: 'inline',
+		lineHeight: 'none',
+		zIndex: '2',
+		pr: 2,
+	}),
+	searchFilterTitle: defineStyle({
+		fontSize: '3xl',
+		mb: 6,
+		w: 'full',
+		borderBottom: '2px',
+		borderColor: 'gray.600',
+	}),
+};
 
 export default defineStyleConfig({
-	baseStyle: {
-		mb: 2,
-		fontWeight: 'normal',
-	},
-	variants: {
-		pageTitle: {
-			fontSize: '4xl',
-		},
-		pageSubtitle: {
-			fontSize: '3xl',
-		},
-		contentTitle: {
-			fontSize: '2xl',
-			pb: 0,
-		},
-		contentSubtitle: {
-			fontSize: 'md',
-			fontWeight: 'medium',
-			mt: 0,
-			pb: 0,
-		},
-		centerline: {
-			size: 'lg',
-			_light: {
-				bg: 'gray.50',
-			},
-			_dark: {
-				bg: 'gray.900',
-				color: 'text.light',
-			},
-			display: 'inline',
-			lineHeight: 'none',
-			zIndex: '2',
-			pr: 2,
-		},
-		searchFilterTitle: {
-			fontSize: '3xl',
-			mb: 6,
-			w: 'full',
-			borderBottom: '2px',
-			borderColor: 'gray.600',
-		},
-	},
+	baseStyle,
+	variants,
 });
