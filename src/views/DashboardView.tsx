d898b9e93bfda523e93b4538a177d89874c6b967
Nick Gaswirth <nick@roundhouse-designs.com>
2023-06-15 17:07:19 -0400

    Dashboard styling

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,8 +1,9 @@
-import { Card, Box, Heading, Stack, Button, Divider, Flex } from '@chakra-ui/react';
+import { Card, Box, Heading, Button, Flex, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import parse from 'html-react-parser';
 import useViewer from '../hooks/queries/useViewer';
 import useGlobalNotices from '../hooks/queries/useGlobalNotices';
+import { FiEdit3, FiUser } from 'react-icons/fi';
 
 export default function DashboardView() {
 	const { loggedInSlug } = useViewer();
@@ -11,18 +12,25 @@ export default function DashboardView() {
 	return (
 		<>
 			<Card mt={4}>
-				<Heading as='h3' variant='contentTitle' mb={0}>
-					To start a Search, use the button in the header!
-				</Heading>
-				<Divider mb={4} />
-				<Stack direction={['column', 'row']} spacing={4} alignItems={['left', 'center']}>
-					<Button as={Link} to={`/profile/${loggedInSlug}`} colorScheme='green' my={0}>
-						View your profile
-					</Button>
-					<Button as={Link} to='/profile/edit' colorScheme='green' my={0}>
-						Edit your profile
-					</Button>
-				</Stack>
+				<Flex justifyContent='space-between' flexWrap='wrap'>
+					<Text fontSize='xl' mb={0}>
+						To start a Search, use the button in the header!
+					</Text>
+					<Flex gap={4} w='full' flexWrap='wrap' my={4}>
+						<Button
+							as={Link}
+							leftIcon={<FiUser />}
+							to={`/profile/${loggedInSlug}`}
+							colorScheme='blue'
+							my={0}
+						>
+							View your profile
+						</Button>
+						<Button as={Link} leftIcon={<FiEdit3 />} to='/profile/edit' colorScheme='green' my={0}>
+							Edit your profile
+						</Button>
+					</Flex>
+				</Flex>
 				{/* <Box mt={4}>
 					<Heading as='h4' variant='contentTitle'>Need a little guidance?</Heading>
 					<Button as={Link} to='/help' colorScheme='orange'>
@@ -31,21 +39,27 @@ export default function DashboardView() {
 				</Box> */}
 			</Card>
 			{notices && notices.length > 0 && (
-				<Flex flexWrap='wrap'>
-					{notices.map((notice: any) => (
-						<Card
-							mt={4}
-							flex='0 0 auto'
-							w={{ base: 'full', sm: 'auto' }}
-							maxW={{ base: 'none', sm: 400 }}
-						>
-							<Heading as='h4' variant='contentSubtitle' key={notice.id}>
-								{notice.title}
-							</Heading>
-							<Box key={notice.id}>{parse(notice.content)}</Box>
-						</Card>
-					))}
-				</Flex>
+				<>
+					<Heading as='h3' variant='contentTitle' mt={4}>
+						Notices!
+					</Heading>
+					<Flex flexWrap='wrap' gap={4}>
+						{notices.map((notice: any) => (
+							<Card
+								key={notice.id}
+								my={0}
+								flex='0 0 auto'
+								w={{ base: 'full', sm: 'auto' }}
+								maxW={{ base: 'none', sm: 400 }}
+							>
+								<Heading as='h4' variant='contentSubtitle'>
+									{notice.title}
+								</Heading>
+								<Box>{parse(notice.content)}</Box>
+							</Card>
+						))}
+					</Flex>
+				</>
 			)}
 		</>
 	);
