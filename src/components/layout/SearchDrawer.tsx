0341226a1367a1e4cbb4002f8a83a47eec84a6c5
Nick Gaswirth <nick@roundhouse-designs.com>
2023-06-27 08:05:56 -0400

    drawer height fix

--- src/components/layout/SearchDrawer.tsx
+++ src/components/layout/SearchDrawer.tsx
@@ -102,7 +102,7 @@ export default function SearchDrawer({ isOpen, onClose }: Props) {
 	};
 
 	return (
-		<Drawer isOpen={isOpen} onClose={onClose} placement='top' size={name ? 'auto' : 'full'}>
+		<Drawer isOpen={isOpen} onClose={onClose} placement='top' size={name ? 'auto' : '100svh'}>
 			<DrawerOverlay _dark={{ bg: 'text.light' }} _light={{ bg: 'text.dark' }} />
 			<DrawerContent>
 				<DrawerHeader
