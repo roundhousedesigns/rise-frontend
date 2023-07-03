7e7218223888d27b7f508bcd5ac5bd1fb5b641a5
Nick Gaswirth <nick@roundhouse-designs.com>
2023-07-03 09:45:18 -0400

    Fix: jump to credits button ref

--- src/routes/EditProfile.tsx
+++ src/routes/EditProfile.tsx
@@ -2,33 +2,34 @@ import { Button, Spinner } from '@chakra-ui/react';
 import Page from '../components/Page';
 import EditProfileView from '../views/EditProfileView';
 import ErrorAlert from '../components/common/ErrorAlert';
+import { forwardRef, useRef } from 'react';
 
 import { EditProfileContextProvider } from '../context/EditProfileContext';
 import useUserProfile from '../hooks/queries/useUserProfile';
 import useViewer from '../hooks/queries/useViewer';
 
-const JumpToCreditsButton = () => {
-	const credits = document.getElementById('credits');
+const JumpToCreditsButton = forwardRef<HTMLButtonElement, {}>((props, ref) => {
+	const handleClick = () => {
+		const creditsElement = document.getElementById('credits');
+		if (creditsElement) {
+			creditsElement.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+		}
+	};
+
 	return (
-		<Button
-			onClick={() =>
-				credits?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
-			}
-			title='Scroll to Credits'
-			textDecoration='none'
-		>
+		<Button onClick={handleClick} ref={ref} title='Scroll to Credits' textDecoration='none'>
 			Jump to Credits
 		</Button>
 	);
-};
+});
 
 export default function EditProfile() {
 	const { loggedInId: userId } = useViewer();
-
 	const [profile, { loading, error }] = useUserProfile(Number(userId));
+	const ref = useRef<HTMLButtonElement>(null);
 
 	return (
-		<Page title={'Update Profile'} actions={<JumpToCreditsButton />}>
+		<Page title={'Update Profile'} actions={<JumpToCreditsButton ref={ref} />}>
 			<EditProfileContextProvider initialState={profile}>
 				{profile && !loading && !error ? (
 					<EditProfileView profile={profile} profileLoading={loading} />
