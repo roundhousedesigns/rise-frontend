dcb4a467f87baa1210d69a1f893690b67055b7d9
Nick Gaswirth <nick@roundhouse-designs.com>
2023-08-09 14:23:25 -0400

    emerg login debug: restore deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { dependencies } from './package.json';
 
 // FIXME VSCode can't follow the alias here for autocomplete, definitions, etc
 // import * as path from 'path';
-
-function renderChunks(deps: Record<string, string>) {
-	let chunks = {};
-	Object.keys(deps).forEach((key) => {
-		if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
-		chunks[key] = [key];
-	});
-	return chunks;
-}
 
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
 	},
-	// build: {
-	// 	rollupOptions: {
-	// 		output: {
-	// 			manualChunks: {
-	// 				vendor: ['react', 'react-router-dom', 'react-dom'],
-	// 				...renderChunks(dependencies),
-	// 			},
-	// 		},
-	// 	},
-	// },
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				},
+			},
+		},
+	},
 	// resolve: {
 	// 	alias: {
 	// 		"@": path.resolve(__dirname, "./src"),
