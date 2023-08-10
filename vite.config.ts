64a50305b1a77a05d9736cbad1e50ce017b34b08
Nick Gaswirth <nick@roundhouse-designs.com>
2023-08-09 21:14:17 -0400

    Chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { dependencies } from './package.json';
 
 // FIXME VSCode can't follow the alias here for autocomplete, definitions, etc
 // import * as path from 'path';
+
+function renderChunks(deps: Record<string, string>) {
+	let chunks = {};
+	Object.keys(deps).forEach((key) => {
+		if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+		chunks[key] = [key];
+	});
+	return chunks;
+}
 
 export default defineConfig({
 	plugins: [react()],
@@ -12,10 +22,9 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						return id.toString().split('node_modules/')[1].split('/')[0].toString();
-					}
+				manualChunks: {
+					vendor: ['react', 'react-router-dom', 'react-dom'],
+					...renderChunks(dependencies),
 				},
 			},
 		},
